5eefa9fe6a263458a71a5f3f9ce8ebad9936bfae
Myo T Kyaw <myotkyaw@gmail.com>
2016-04-14 03:42:00 +0000

    Add route to show answers index

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'static_pages#index'
+  
+  get 'answers' => 'answers#index'
 
 
   # Example of regular route:
