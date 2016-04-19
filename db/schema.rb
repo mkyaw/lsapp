741cdde412f7afdab5eacec4d843878213018bc4
Myo T Kyaw <myotkyaw@gmail.com>
2016-04-19 17:46:03 +0000

    Remove standalone Answer-related model and tables

--- db/schema.rb
+++ db/schema.rb
@@ -11,23 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160419172910) do
+ActiveRecord::Schema.define(version: 20160419174334) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "answers", force: :cascade do |t|
-    t.string   "course"
-    t.string   "professor"
-    t.text     "answer_one"
-    t.text     "answer_two"
-    t.text     "answer_three"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
-    t.integer  "user_id"
-  end
-
-  add_index "answers", ["user_id"], name: "index_answers_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "",    null: false
