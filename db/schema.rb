2725a40ca67b1ff10f81808b24c51076848ca1a8
Myo T Kyaw <myotkyaw@gmail.com>
2016-04-19 01:32:38 +0000

    Create relationship between Answer and User models

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160419010830) do
+ActiveRecord::Schema.define(version: 20160419012910) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,7 +24,10 @@ ActiveRecord::Schema.define(version: 20160419010830) do
     t.text     "answer_three"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.integer  "user_id"
   end
+
+  add_index "answers", ["user_id"], name: "index_answers_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
